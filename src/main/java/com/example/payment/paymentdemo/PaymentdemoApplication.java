65cec720eb87dfd63f0d11691be3007665d97492
AM <alexandr.makarevich@qrok.mobi>
2018-10-26 17:26:40 +0300

    Initial commit

---
+++ src/main/java/com/example/payment/paymentdemo/PaymentdemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.payment.paymentdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentdemoApplication.class, args);
+	}
+}
